2d635bfa77192833025bf3cc8880b04f47a411a9
alundyz <88989587+alundyz@users.noreply.github.com>
2022-10-28 13:27:39 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -47,7 +47,7 @@ class AI(object):
         # You need to add your decision to your candidate_list. The system will get the end of your candidate_list as your decision.
         self.candidate_list = []
         self.coefficient_list = coefficient_list
-        self.time_stamp = np.array([14,25])
+        self.time_stamp = np.array([14,28])
         self.search_method = 'alpha_beta'
         self.height = 2
         self.cnt = 0
@@ -573,6 +573,29 @@ def compete(coefficient_all):
         res_list[i] += agent_list2[i].win_cnt
     print(res_list)
     return res_list
+def compete2(coefficient_all,coefficient_elite):
+    agent_list = []
+    for i in coefficient_all:
+        agent = AI(1,i)
+        agent_list.append(agent)
+    
+    agent_list2 = []
+    for i in coefficient_elite:
+        agent = AI(-1,i)
+        agent_list2.append(agent)
+
+    res_list = np.zeros(len(agent_list),int)
+    num_cnt = 0
+    for agent_a in agent_list:
+        print(num_cnt)
+        for agent_b in agent_list2:
+            num_cnt += 1
+            test(agent_a,agent_b)
+    
+    for i in range(len(res_list)):
+        res_list[i] += agent_list[i].win_cnt
+    print(res_list)
+    return res_list
 def initialize(Population):
     coefficient_rand = np.random.rand(Population,3,5)
     coefficient_all = []
@@ -584,9 +607,11 @@ def initialize(Population):
 def born(father_coefficient,mother_coefficient):
     child_coefficient = np.zeros((3,5))
     rand_list = np.random.rand(3)
+    # print(mother_coefficient[0])
     for i in range(len(rand_list)):
         if i < 0.3333:
             child_coefficient[i] = mother_coefficient[i]
+            
             n1 = np.random.randint(5)
             n2 = np.random.rand(1)[0]
             if n2 > 0.8:
@@ -614,22 +639,31 @@ def reproduce(elite_coefficient):
     return coefficient_all
 
 def evolution(Population,elite,max_generation):
-    print("________________generation_____________",0)
-    coefficient_all = initialize(Population)
-    res_list = compete(coefficient_all)
-    idx = np.argsort(res_list)
-    coefficient_all = np.array(coefficient_all)
-    sorted_coefficient = coefficient_all[idx]
-    elite_coefficient = sorted_coefficient[-elite:]
-    name = "results/generation_0.txt"
-    # print("len",len(elite_coefficient))
-    with open(name,'ab') as f:
-        for i in elite_coefficient:
-            np.savetxt(f, i,delimiter=" ")
-    for i in range(1,max_generation):
+    elite_coefficient = np.array([[[-1.538797003201547442e-01, 1.638966205349210270e+01, 2.342724698678185113e+00, 1.654648849346949291e+01, 1.603830293034772581e+01],
+[8.874064918156966408e-01, 1.662736560774202843e+01, 9.570886650289157638e-01, 2.455612943761423494e+01, 2.520898472322592454e+01],
+[1.002200645270865920e+02, 3.045451347828674216e+01, 1.590147001865654497e+00, 2.606476589512995901e+01, 2.494094302908655791e+01]],
+[[1.130897134362724543e+00, 1.643120832640492068e+01, 2.843675876857774565e+00, 1.535582833629146116e+01, 1.555475302783436753e+01],
+[9.286446530626227824e-01, 1.527093750298321062e+01, 1.089501630356933370e+00, 2.522456962423906646e+01, 2.543472529562980000e+01],
+[1.003555740930313931e+02, 3.022152912637647759e+01, 1.413440380086795622e+00, 2.565498082366150001e+01, 2.573636885362022753e+01]],
+[[5.605259783665857976e-01, 1.559320683498615701e+01, 3.037606087542521927e+00, 1.430273775356920396e+01, 1.605138119298578303e+01],
+[5.033907402160214817e-02, 1.614099114133242097e+01, 1.200841227100089004e+00, 2.564757853410176125e+01, 2.491766696790871194e+01],
+[1.003255734179346206e+02, 3.121493967120985857e+01, 2.049061589835561303e+00, 2.529791834682720264e+01, 2.574143375439256332e+010]],
+[[4.298101116913769504e-01, 1.647313731115948698e+01, 2.813588580569891473e+00, 1.510962122116071882e+01, 1.590123070784127535e+01],
+[4.533727064276672492e-01, 1.504068384828493166e+01, 1.044836602779467105e+00, 2.566809161718384047e+01, 2.538541402085456866e+01],
+[9.998102929911982528e+01, 3.037669950272054464e+01, 1.586117637130932678e+00, 2.550392449424832719e+01, 2.595687668937362957e+01]],
+[[3.883926282235586891e-01, 1.519772067139832750e+01, 2.615843446796007221e+00, 1.587491369903422189e+01, 1.541629763981949353e+01],
+[2.780089100260244583e-01, 1.509186945700944271e+01, 1.237677440872410273e+00, 2.590257772351219856e+01, 2.594100589949387015e+01],
+[1.004325288595769337e+02, 3.106020268512924076e+01, 1.917376856944105867e+00 ,2.564421616278460547e+01, 2.525054283553376777e+01]],
+[[4.925461873166057103e-01, 1.607822330209225825e+01, 2.752596448409076579e+00, 1.462065397195638994e+01, 1.628309157854537759e+01],
+[6.296833349016023318e-01, 1.626637795492203153e+01, 1.312942354869150563e+00, 2.486054889819170199e+01, 2.593196754676552729e+01],
+[1.000300716032054709e+02, 3.103281508254622167e+01, 1.656961802620078528e+00, 2.531488018012895580e+01, 2.517814580584203199e+01]],
+[[1.549224030610849123e+00, 1.617185782353969969e+01, 2.933023493678870608e+00, 1.515098431234881282e+01, 1.577202966482161983e+01],
+[5.942082197305617042e-01, 1.598243440766308332e+01, 9.001473561353465769e-01, 2.483357603924312684e+01, 2.591841314920721118e+01],
+[1.005843969900493704e+02, 3.073259939827065423e+01, 1.859333162528225003e+00, 2.562268514541733211e+01, 2.551548276951523420e+01]]])
+    for i in range(3,max_generation):
         print("___________________generation____________",i)
         coefficient_all = reproduce(elite_coefficient)
-        res_list = compete(coefficient_all)
+        res_list = compete2(coefficient_all,elite_coefficient)
         idx = np.argsort(res_list)
         coefficient_all = np.array(coefficient_all)
         sorted_coefficient = coefficient_all[idx]
@@ -647,10 +681,13 @@ if __name__ == '__main__':
     c = [[2.667443226210113316e-01 ,1.680531890176057885e+01, 2.075904244318421910e+00 ,1.576418865146350612e+01 ,1.501548590595926669e+01],
 [9.044118520020477670e-01, 1.593580914214212996e+01, 2.235151037514111927e+00 ,2.594413766754942330e+01, 2.545640259733475119e+01],
 [1.005308968814381956e+02 ,3.076034270791147662e+01, 1.365055235016227941e+00, 2.500050777235150079e+01, 2.539702096028404199e+01]]
-    agent_b = AI(1,c)
-    agent_a = AI(-1,[[0,16,1.5,15,15],[0,15,1.3,25,25],[100,30,1,25,25]])
-    test(agent_a,agent_b)
-    print(agent_a.win_cnt)
-    print(agent_b.win_cnt)
+    # t1 = time.time()
+    # agent_b = AI(1,c)
+    # agent_a = AI(-1,[[0,16,1.5,15,15],[0,15,1.3,25,25],[100,30,1,25,25]])
+    # test(agent_a,agent_b)
+    # t2 = time.time()
+    # print(t2-t1)
+    # print(agent_a.win_cnt)
+    # print(agent_b.win_cnt)
 
 
